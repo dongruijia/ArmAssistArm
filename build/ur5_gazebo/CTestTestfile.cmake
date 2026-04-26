321c2783bc4bda4dd0d2839c17009c859d210850
jiaid <3271302032@qq.com>
2026-04-26 21:13:46 +0800

    delet local data

--- build/ur5_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiaid/ur5_rehab_ws/src/ur5_gazebo
-# Build directory: /home/jiaid/ur5_rehab_ws/build/ur5_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
